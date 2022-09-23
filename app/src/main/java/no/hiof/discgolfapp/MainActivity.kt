32a9fa2d839401f1325e0e073b4ae47100776657
vetlewj <vetlewj@gmail.com>
2022-09-23 14:30:50 +0200

    Bottom Navigation for Courses, Play and MyDiscs

--- app/src/main/java/no/hiof/discgolfapp/MainActivity.kt
+++ app/src/main/java/no/hiof/discgolfapp/MainActivity.kt
@@ -2,10 +2,21 @@ package no.hiof.discgolfapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import no.hiof.discgolfapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+
+        binding.navView.setupWithNavController(navHostFragment.navController)
     }
 }
