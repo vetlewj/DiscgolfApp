88c5fd446f699abe92526644ff765f809027bd0d
Kristiankaspersen <kristian.kaspersen@hiof.no>
2022-09-29 01:18:45 +0200

    Lagt inn de 3 første banene i API'et som inneholder informasjon om baner inn i Course modell klassen

--- app/src/main/java/no/hiof/discgolfapp/model/Course.kt
+++ app/src/main/java/no/hiof/discgolfapp/model/Course.kt
@@ -1,5 +1,26 @@
 package no.hiof.discgolfapp.model
 
-data class Course(val name: String) {
+data class Course(
+    val name: String,
+    val holes: Int?,
+    val rating: Double?,
+    val area: String?,
+    val city: String?,
+    val location: String?,
+    val latitude: Double?,
+    val longitude: Double?
+) {
     //TODO: Finish class
+
+    companion object {
+        fun getCourses(): List<Course> {
+            // Hentet data rett fra API
+            return listOf(
+                Course("Muselunden", null, null, "Oslo", "Oslo", null, 59.939369, 10.785842),
+                Course("Ekeberg skole", null, null, "Oslo", "Oslo", null, 59.895178, 10.787161),
+                Course("Frogner", null, null, "", null, null, null, null)
+                // TODO: Add more sample data
+            )
+        }
+    }
 }
