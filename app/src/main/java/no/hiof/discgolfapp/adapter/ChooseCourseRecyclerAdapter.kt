20c54b5efd92fe26f91a45777ca1471a28eb9e2b
vetlewj <vetlewj@gmail.com>
2022-10-07 16:42:10 +0200

    Choose course and navigate to createScoreCard

--- app/src/main/java/no/hiof/discgolfapp/adapter/ChooseCourseRecyclerAdapter.kt
+++ app/src/main/java/no/hiof/discgolfapp/adapter/ChooseCourseRecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package no.hiof.discgolfapp.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,17 +9,22 @@ import androidx.recyclerview.widget.RecyclerView
 import no.hiof.discgolfapp.R
 import no.hiof.discgolfapp.model.Course
 
-class ChooseCourseRecyclerAdapter(private val courses: List<Course>) : RecyclerView.Adapter<ChooseCourseRecyclerAdapter.ChooseCourseViewHolder>() {
+class ChooseCourseRecyclerAdapter(private val courses: List<Course>, private val clickListener: View.OnClickListener) : RecyclerView.Adapter<ChooseCourseRecyclerAdapter.ChooseCourseViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChooseCourseViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.choose_course_list_item, parent, false)
+
+        Log.d("ChooseCourseAdapter", "onCreateViewHolder")
+
         return ChooseCourseViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ChooseCourseViewHolder, position: Int) {
         val currentCourse = courses[position]
 
-        holder.bind(currentCourse)
+        Log.d("ChooseCourseRecycler", "onBindViewHolder: ${currentCourse.name}")
+
+        holder.bind(currentCourse, clickListener)
     }
 
     override fun getItemCount(): Int {
@@ -28,8 +34,10 @@ class ChooseCourseRecyclerAdapter(private val courses: List<Course>) : RecyclerV
     class ChooseCourseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val chooseCourseBtn : Button = view.findViewById(R.id.chooseCourseBtn)
 
-        fun bind(course: Course) {
+        fun bind(course: Course, clickListener: View.OnClickListener) {
             chooseCourseBtn.text = course.name
+            chooseCourseBtn.setOnClickListener(clickListener)
+            Log.d("ChooseCourseAdapter", "bind: ${course.name}")
         }
 
     }
--- app/src/main/java/no/hiof/discgolfapp/screens/createscorecard/ChooseCourseFragment.kt
+++ app/src/main/java/no/hiof/discgolfapp/screens/createscorecard/ChooseCourseFragment.kt
@@ -1,10 +1,13 @@
 package no.hiof.discgolfapp.screens.createscorecard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import no.hiof.discgolfapp.R
@@ -12,6 +15,8 @@ import no.hiof.discgolfapp.adapter.ChooseCourseRecyclerAdapter
 import no.hiof.discgolfapp.model.Course
 
 class ChooseCourseFragment : Fragment() {
+
+    private val courseList : List<Course> = Course.getCourses()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,7 +31,17 @@ class ChooseCourseFragment : Fragment() {
         val courseRecyclerView = view.findViewById<RecyclerView>(R.id.choose_course_recycler_view)
 
         // TODO: Courses should be sorted by distance from user
-        courseRecyclerView.adapter = ChooseCourseRecyclerAdapter(Course.getCourses())
+        courseRecyclerView.adapter = ChooseCourseRecyclerAdapter(courseList){
+            Log.d("ChooseCourseFragment", "Course clicked")
+            // TODO: Get the course clicked and pass it to the next fragment
+            val courseNum = 1
+            val action = ChooseCourseFragmentDirections.actionChooseCourseFragmentToCreateScoreCardFragment(courseNum)
+            action.courseNum = courseNum
+
+            Toast.makeText(context, "Course ${courseNum+1} selected", Toast.LENGTH_SHORT).show()
+
+            findNavController().navigate(action)
+        }
 
         courseRecyclerView.layoutManager = GridLayoutManager(context, 1)
 
