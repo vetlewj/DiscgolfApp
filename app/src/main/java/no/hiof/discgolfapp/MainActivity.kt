0b70fe3d17fd932e85ecabe4369d14aa00d1d03e
vetlewj <vetlewj@gmail.com>
2022-10-11 22:44:59 +0200

    Navigation drawer progress

--- app/src/main/java/no/hiof/discgolfapp/MainActivity.kt
+++ app/src/main/java/no/hiof/discgolfapp/MainActivity.kt
@@ -3,11 +3,13 @@ package no.hiof.discgolfapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import no.hiof.discgolfapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        val drawerLayout = binding.drawerLayout
 
-        binding.bottomNavView.setupWithNavController(navHostFragment.navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
