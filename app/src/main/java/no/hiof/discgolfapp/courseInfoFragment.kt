02579eb01a7d8e0a8f2004f9c344163a77bec9ef
Kristiankaspersen <kristiankasp@gmail.com>
2022-10-13 14:39:01 +0200

    autogenerated the files instead

---
+++ app/src/main/java/no/hiof/discgolfapp/courseInfoFragment.kt
@@ -0,0 +1,23 @@
+package no.hiof.discgolfapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class courseInfoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_course_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/no/hiof/discgolfapp/screens/courseInfoFragment.kt
+++
@@ -1,4 +0,0 @@
-package no.hiof.discgolfapp.screens
-
-class courseInfoFragment {
-}
