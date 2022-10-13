fd3f5c0b02f74c11aebfdfee2b7e432a7a2e159c
Kristiankaspersen <kristiankasp@gmail.com>
2022-10-14 00:40:45 +0200

    have added an action from infoFrag to createScoreCardFrag, now you can nav between them

--- app/src/main/java/no/hiof/discgolfapp/CourseInfoFragment.kt
+++ app/src/main/java/no/hiof/discgolfapp/CourseInfoFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import no.hiof.discgolfapp.databinding.FragmentCourseInfoBinding
 
 
 class CourseInfoFragment : Fragment() {
-
+    private val args: CourseInfoFragmentArgs by navArgs()
     private var fragmentBinding: FragmentCourseInfoBinding? = null
 
     override fun onCreateView(
@@ -24,6 +26,18 @@ class CourseInfoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentCourseInfoBinding.bind(view)
         fragmentBinding = binding
+
+        binding.createScoreCardInfobutton.setOnClickListener() {
+            val navController = this.findNavController()
+
+            val action = CourseInfoFragmentDirections.actionCourseInfoFragmentToCreateScoreCardFragment(args.courseName)
+
+            navController.navigate(action)
+
+
+        }
+
+
 
 
     }
