d378cf9199d408b7dc3ed39e6b0e28015be5a2a2
Kristiankaspersen <kristiankasp@gmail.com>
2022-10-13 15:34:41 +0200

    in last commit i added action, with uid argument from click, and navigated using navController

--- app/src/main/java/no/hiof/discgolfapp/CoursesOverviewListFragment.kt
+++ app/src/main/java/no/hiof/discgolfapp/CoursesOverviewListFragment.kt
@@ -40,6 +40,7 @@ class CoursesOverviewListFragment : Fragment() {
             val action = CoursesOverviewListFragmentDirections.actionCoursesOverviewListFragmentToCourseInfoFragment()
             action.uid = selectedCourse.uid
 
+
             findNavController().navigate(action)
 
             Toast.makeText(view.context, selectedCourse.name + " clicked", Toast.LENGTH_SHORT).show()
