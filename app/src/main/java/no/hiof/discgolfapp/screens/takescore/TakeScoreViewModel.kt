ad76af4da8f9efd504998d6407db4caa59cf869a
vetlewj <vetlewj@gmail.com>
2022-10-14 14:29:00 +0200

    Implement viewModel

---
+++ app/src/main/java/no/hiof/discgolfapp/screens/takescore/TakeScoreViewModel.kt
@@ -0,0 +1,15 @@
+package no.hiof.discgolfapp.screens.takescore
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class TakeScoreViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+    var score = 0
+    var par = 0
+    var holeNumber = 0
+
+    init {
+        Log.i("TakeScoreViewModel", "TakeScoreViewModel created")
+    }
+}
