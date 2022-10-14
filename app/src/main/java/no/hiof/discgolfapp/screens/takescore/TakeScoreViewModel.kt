99a0b09cc39c01478a1881c995ca59503c5f8624
vetlewj <vetlewj@gmail.com>
2022-10-14 16:59:50 +0200

    Add scorecard to viewmodel

--- app/src/main/java/no/hiof/discgolfapp/screens/takescore/TakeScoreViewModel.kt
+++ app/src/main/java/no/hiof/discgolfapp/screens/takescore/TakeScoreViewModel.kt
@@ -2,9 +2,11 @@ package no.hiof.discgolfapp.screens.takescore
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import no.hiof.discgolfapp.model.ScoreCard
 
 class TakeScoreViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    var scoreCard : ScoreCard? = null
     var score = 0
     var par = 0
     var holeNumber = 0
