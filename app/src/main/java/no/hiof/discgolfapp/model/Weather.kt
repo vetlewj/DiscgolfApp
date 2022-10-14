d6d0a27ba49417c623045c23927f44cede719584
Kristiankaspersen <kristiankasp@gmail.com>
2022-10-14 23:51:49 +0200

    made a function for finding weather based on coordinates

--- app/src/main/java/no/hiof/discgolfapp/model/Weather.kt
+++ app/src/main/java/no/hiof/discgolfapp/model/Weather.kt
@@ -3,13 +3,25 @@ package no.hiof.discgolfapp.model
 import android.graphics.drawable.Drawable
 import no.hiof.discgolfapp.R
 
-class Weather(private val symbolCode: WeatherSymbol, private var temperature: Double, private var windspeed: Double, private var windFromDirection: Double, private var lat: Double, private var lon: Double, private var time: String) {
+class Weather(private val symbolCode: WeatherSymbol?, private var temperature: Double?, private var windspeed: Double?, private var windFromDirection: Double?, private var lat: Double?, private var lon: Double?, private var time: String?) {
 
     companion object {
 
-        fun getWeather() {
-            Weather(WeatherSymbol.SUNNY, 14.6, 3.3, 215.4, 59.939369, 10.785842, "2022-10-14T13:00:00Z")
-            Weather(WeatherSymbol.CLOUDY, 10.2, 4.2, 213.2, 59.895178, 10.787161, "2022-10-14T12:00:00Z")
+        fun getWeather(): List<Weather> {
+            return listOf(
+                Weather(WeatherSymbol.SUNNY, 14.6, 3.3, 215.4, 59.939369, 10.785842, "2022-10-14T13:00:00Z"),
+                Weather(WeatherSymbol.CLOUDY, 10.2, 4.2, 213.2, 59.895178, 10.787161, "2022-10-14T12:00:00Z")
+            )
+        }
+
+        fun getWeatherFromCoordinate(latitude: Double, longitude: Double): Weather {
+
+            for (weather in getWeather()) {
+                if (weather.lat == latitude && weather.lon == longitude) {
+                    return weather
+                }
+            }
+            return Weather(null, null, null, null, null, null, null)
         }
 
 
