df0af1e0f166d51f146d4cc7c46f4c50ca553a82
Kristiankaspersen <kristiankasp@gmail.com>
2022-10-15 02:48:53 +0200

    now windspeed and temp shows dynamically ,changed lon and lat to float in course and weather model

--- app/src/main/java/no/hiof/discgolfapp/model/Weather.kt
+++ app/src/main/java/no/hiof/discgolfapp/model/Weather.kt
@@ -3,29 +3,30 @@ package no.hiof.discgolfapp.model
 import android.graphics.drawable.Drawable
 import no.hiof.discgolfapp.R
 
-class Weather(private val symbolCode: WeatherSymbol?, private var temperature: Double?, private var windspeed: Double?, private var windFromDirection: Double?, private var lat: Double?, private var lon: Double?, private var time: String?) {
+class Weather(private val symbolCode: WeatherSymbol?, var temperature: Double?, var windspeed: Double?, private var windFromDirection: Double?, private var lat: Float?, private var lon: Float?, private var time: String?) {
 
     companion object {
 
         fun getWeather(): List<Weather> {
             return listOf(
-                Weather(WeatherSymbol.SUNNY, 14.6, 3.3, 215.4, 59.939369, 10.785842, "2022-10-14T13:00:00Z"),
-                Weather(WeatherSymbol.CLOUDY, 10.2, 4.2, 213.2, 59.895178, 10.787161, "2022-10-14T12:00:00Z")
+                Weather(WeatherSymbol.SUNNY, 14.6, 3.3, 215.4, 59.939369F, 10.785842F, "2022-10-14T13:00:00Z"),
+                Weather(WeatherSymbol.CLOUDY, 10.2, 4.2, 213.2, 59.895178F, 10.787161F, "2022-10-14T12:00:00Z")
             )
         }
 
-        fun getWeatherFromCoordinate(latitude: Double, longitude: Double): Weather {
-
+        fun getWeatherFromCoordinate(latitude: Float, longitude: Float): Weather {
+            // TODO: find a better way of doing this
             for (weather in getWeather()) {
                 if (weather.lat == latitude && weather.lon == longitude) {
                     return weather
                 }
+
             }
-            return Weather(null, null, null, null, null, null, null)
+            return Weather(null, 2.2, null, null, null, null, null)
         }
 
-
     }
+
 
     fun getWeatherSymbol() {
         val weatherFavIcon = when (this.symbolCode) {
