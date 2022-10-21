727628d9a99c3cab7f4280af537928965f84f84f
vetlewj <vetlewj@gmail.com>
2022-10-22 00:13:03 +0200

    request location permission

--- app/src/main/java/no/hiof/discgolfapp/screens/maps/CourseMapsFragment.kt
+++ app/src/main/java/no/hiof/discgolfapp/screens/maps/CourseMapsFragment.kt
@@ -1,12 +1,19 @@
 package no.hiof.discgolfapp.screens.maps
 
-import androidx.fragment.app.Fragment
-
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,27 +23,60 @@ import com.google.android.gms.maps.model.MarkerOptions
 import no.hiof.discgolfapp.R
 
 class CourseMapsFragment : Fragment() {
+    private val TAG = "CourseMapsFragment"
+    private lateinit var map: GoogleMap
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     // TODO: Add navigation back to overview when clicking on switch
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                // Precise location access granted.
+                Log.d(TAG, "Precise location access granted")
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // Only approximate location access granted.
+                Log.d(TAG, "Only approximate location access granted")
+            } else -> {
+            // No location access granted.
+                Log.d(TAG, "No location access granted")
+        }
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        // TODO: Fix start location
+
+        map = googleMap
+        map.mapType = GoogleMap.MAP_TYPE_HYBRID
+        map.uiSettings.isZoomControlsEnabled = true
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+            Log.d(TAG, "Location: $location")
+            // TODO: Fix location is null here
+            if (location != null) {
+                val currentLocation = LatLng(location.latitude, location.longitude)
+                map.addMarker(MarkerOptions().position(currentLocation).title("Marker in current location"))
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
+            }
+        }
+        // TODO: Get Current location and set as start location
+
         // TODO: Add markers for each course
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
-        googleMap.uiSettings.isZoomControlsEnabled = true
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        googleMap.moveCamera(CameraUpdateFactory.zoomTo(10F))
+
     }
 
     override fun onCreateView(
