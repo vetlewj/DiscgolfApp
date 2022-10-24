cdbe0a0e82c9a5478a6b4825f77c04213981e677
vetlewj <vetlewj@gmail.com>
2022-10-24 18:34:08 +0200

    Navigate back to course list when clicking switch

--- app/src/main/java/no/hiof/discgolfapp/screens/maps/CourseMapsFragment.kt
+++ app/src/main/java/no/hiof/discgolfapp/screens/maps/CourseMapsFragment.kt
@@ -9,9 +9,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -20,6 +22,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.switchmaterial.SwitchMaterial
 import no.hiof.discgolfapp.R
 
 class CourseMapsFragment : Fragment() {
@@ -28,7 +31,6 @@ class CourseMapsFragment : Fragment() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    // TODO: Add navigation back to overview when clicking on switch
 
     @RequiresApi(Build.VERSION_CODES.N)
     val locationPermissionRequest = registerForActivityResult(
@@ -42,10 +44,11 @@ class CourseMapsFragment : Fragment() {
             permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
                 // Only approximate location access granted.
                 Log.d(TAG, "Only approximate location access granted")
-            } else -> {
-            // No location access granted.
+            }
+            else -> {
+                // No location access granted.
                 Log.d(TAG, "No location access granted")
-        }
+            }
         }
     }
 
@@ -58,18 +61,23 @@ class CourseMapsFragment : Fragment() {
         map.mapType = GoogleMap.MAP_TYPE_HYBRID
         map.uiSettings.isZoomControlsEnabled = true
 
-        locationPermissionRequest.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION))
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 
-        fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
             Log.d(TAG, "Location: $location")
             // TODO: Fix location is null here
             if (location != null) {
                 val currentLocation = LatLng(location.latitude, location.longitude)
-                map.addMarker(MarkerOptions().position(currentLocation).title("Marker in current location"))
+                map.addMarker(
+                    MarkerOptions().position(currentLocation).title("Marker in current location")
+                )
                 map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
             }
         }
@@ -87,9 +95,23 @@ class CourseMapsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_course_maps, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val mapSwitch = view.findViewById<SwitchMaterial>(R.id.mapsToCoursesListSwitch)
+        mapSwitch?.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Log.d(TAG, "Switch clicked")
+                val action =
+                    CourseMapsFragmentDirections.actionCourseMapsFragmentToCoursesOverviewListFragment()
+                view.findNavController().navigate(action)
+            }
+        }
+
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+
     }
 }
