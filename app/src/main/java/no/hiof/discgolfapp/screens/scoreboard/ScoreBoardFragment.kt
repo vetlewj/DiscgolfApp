f9ae9832ed74f708f67892c30c28cf2861f0c461
vetlewj <vetlewj@gmail.com>
2022-11-02 22:28:48 +0100

    Navigate to coure overview

--- app/src/main/java/no/hiof/discgolfapp/screens/scoreboard/ScoreBoardFragment.kt
+++ app/src/main/java/no/hiof/discgolfapp/screens/scoreboard/ScoreBoardFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
@@ -52,6 +54,12 @@ class ScoreBoardFragment : Fragment() {
             } else {
                 println("Could not find scorecard")
             }
+        }
+
+        val continueButton = binding.finishScoreBoardButton
+        continueButton.setOnClickListener {
+            val action = ScoreBoardFragmentDirections.actionScoreBoardFragmentToCoursesOverviewListFragment()
+            it.findNavController().navigate(action)
         }
         return binding.root
     }
