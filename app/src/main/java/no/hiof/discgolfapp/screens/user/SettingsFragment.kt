92e5743248d3df919ee4c1ed29d968ceaba90365
vetlewj <vetlewj@gmail.com>
2022-11-03 20:45:55 +0100

    rename fragments to fit naming convention

--- app/src/main/java/no/hiof/discgolfapp/screens/user/SettingsFragment.kt
+++ app/src/main/java/no/hiof/discgolfapp/screens/user/SettingsFragment.kt
@@ -7,14 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import no.hiof.discgolfapp.R
 
-class userFragment : Fragment() {
+class SettingsFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
-
+    
 }
--- app/src/main/java/no/hiof/discgolfapp/screens/user/UserFragment.kt
+++ app/src/main/java/no/hiof/discgolfapp/screens/user/UserFragment.kt
@@ -7,15 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import no.hiof.discgolfapp.R
 
-class settingsFragment : Fragment() {
-
+class UserFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return inflater.inflate(R.layout.fragment_user, container, false)
     }
-    
+
 }
