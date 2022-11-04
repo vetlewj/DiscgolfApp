b2926ba7c52d0ee709550d23aabfa76dc0bf41d9
Børre Stellander <stellander88@gmail.com>
2022-11-04 20:46:13 +0100

    edited profile layout

--- app/src/main/java/no/hiof/discgolfapp/screens/user/ProfileFragment.kt
+++ app/src/main/java/no/hiof/discgolfapp/screens/user/ProfileFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import no.hiof.discgolfapp.R
@@ -16,7 +18,6 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
