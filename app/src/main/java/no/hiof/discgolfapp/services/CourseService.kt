532e3e3a4297ccd66719eeebaf588a1e456e8f76
Kristiankaspersen <kristiankasp@gmail.com>
2022-11-04 19:41:08 +0100

    added a data class for response from course list API

--- app/src/main/java/no/hiof/discgolfapp/services/CourseService.kt
+++ app/src/main/java/no/hiof/discgolfapp/services/CourseService.kt
@@ -1,10 +1,11 @@
 package no.hiof.discgolfapp.services
 
+import no.hiof.discgolfapp.helper.data.GetCoursesByCountryCodeResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface CourseService {
 
     @GET("api.php?content=courses_list&country_code=NO")
-    fun getCoursesByCountryCode(): Call<Any>
+    fun getCoursesByCountryCode(): Call<GetCoursesByCountryCodeResponse>
 }
