f68413bc73cb92f45e1f4116e2f8a93a01047fbf
Kristiankaspersen <kristian.kaspersen@hiof.no>
2022-11-22 00:32:49 +0100

    have added date and time, documentRefUid to users documents in user collection when making user

--- app/src/main/java/no/hiof/discgolfapp/model/User.kt
+++ app/src/main/java/no/hiof/discgolfapp/model/User.kt
@@ -1,14 +1,19 @@
 package no.hiof.discgolfapp.model
 
+import java.util.Date
+
 class User(
-    val uid: Int,
+    val id: Int,
     val guest: Boolean,
+    val dateCreated: Date,
     var name: String?,
     var email: String?,
     var pictureUrl: String?,
     val scoreCards: ArrayList<ScoreCard>?,
     val discs: ArrayList<Disc>?,
     val friends: ArrayList<User>?,
-    val throws: ArrayList<Throw>
+    val throws: ArrayList<Throw>?,
+    val friendsRequests: ArrayList<MutableMap<String, Boolean>>
+
 ) {
 }
