64a7b4c802ff959dc082262b713e9495fd530af7
Børre Stellander <stellander88@gmail.com>
2022-11-23 12:06:39 +0100

    Drawing simple rectangle

--- app/src/main/java/no/hiof/discgolfapp/screens/discs/DiscGridFragment.kt
+++ app/src/main/java/no/hiof/discgolfapp/screens/discs/DiscGridFragment.kt
@@ -1,11 +1,19 @@
 package no.hiof.discgolfapp.screens.discs
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
+import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import no.hiof.discgolfapp.databinding.FragmentDiscGridBinding
+import java.lang.reflect.Modifier
 
 class DiscGridFragment : Fragment()  {
 
@@ -22,6 +30,33 @@ class DiscGridFragment : Fragment()  {
 
         binding = FragmentDiscGridBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val bitmap: Bitmap = Bitmap.createBitmap(700, 1000, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(bitmap)
+
+        var shapeDrawable: ShapeDrawable
+
+        // rectangle positions
+        var left = 30
+        var top = 50
+        var right = 670
+        var bottom = 600
+
+        // draw rectangle shape to canvas
+        shapeDrawable = ShapeDrawable(RectShape())
+        shapeDrawable.setBounds( left, top, right, bottom)
+        shapeDrawable.getPaint().setColor(Color.parseColor("lightgray"))
+        shapeDrawable.draw(canvas)
+
+
+        val imageView = binding.imageView
+        imageView.background = BitmapDrawable(getResources(), bitmap)
     }
 
 
