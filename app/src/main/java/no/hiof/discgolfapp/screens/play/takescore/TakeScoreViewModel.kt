7e8cb147bcc67527c17c4d49aa28899071b3fbb5
vetlewj <vetlewj@gmail.com>
2022-11-24 00:35:24 +0100

    fetch scorecard from firestore

--- app/src/main/java/no/hiof/discgolfapp/screens/play/takescore/TakeScoreViewModel.kt
+++ app/src/main/java/no/hiof/discgolfapp/screens/play/takescore/TakeScoreViewModel.kt
@@ -1,7 +1,15 @@
 package no.hiof.discgolfapp.screens.play.takescore
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.MetadataChanges
+import com.google.firebase.firestore.ktx.toObjects
 import no.hiof.discgolfapp.model.ScoreCard
 
 class TakeScoreViewModel : ViewModel() {
@@ -13,7 +21,31 @@ class TakeScoreViewModel : ViewModel() {
     var totalScore = 0
     var totalPar = 0
 
+    private var firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    private var _scorecard : MutableLiveData<ScoreCard> = MutableLiveData()
+    val storedScoreCard: LiveData<ScoreCard> = _scorecard
+
     init {
         Log.i("TakeScoreViewModel", "TakeScoreViewModel created")
     }
+
+    fun fetchScoreCard(scorecardId: String, context: Context){
+        val storedCard = firestore.collection("scorecards")
+            .whereEqualTo("playerId", firebaseAuth.currentUser?.uid)
+            .whereEqualTo("id", scorecardId)
+        storedCard.addSnapshotListener(MetadataChanges.INCLUDE) {value, error ->
+            if (error != null){
+                Log.d("TakeScore", "Listen failed, could not get scorecard")
+                Toast.makeText(context, "Kunne ikke hente poengkort",Toast.LENGTH_SHORT
+                ).show()
+            }
+            if (value != null){
+                val firestoreCards = value.toObjects<ScoreCard>()
+                _scorecard.postValue(firestoreCards[0])
+            }
+        }
+    }
+
 }
