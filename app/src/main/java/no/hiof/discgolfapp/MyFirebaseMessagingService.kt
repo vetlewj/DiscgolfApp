a897c4b4db5793cfc2bde04497de2a018a948f14
Kristiankaspersen <kristian.kaspersen@hiof.no>
2022-11-24 06:46:28 +0100

    added logic to MyFirebaseMessagingServce, and asking user if they want to receive notifications

--- app/src/main/java/no/hiof/discgolfapp/MyFirebaseMessagingService.kt
+++ app/src/main/java/no/hiof/discgolfapp/MyFirebaseMessagingService.kt
@@ -1,29 +1,67 @@
 package no.hiof.discgolfapp
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.util.Log
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+const val CHANNEL_ID = "notification_channel"
+const val CHANNEL_NAME = "no.hiof.discgolfapp"
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // ...
+    fun getRemoteView(notificationMessage: String): RemoteViews {
+        val remoteView = RemoteViews(CHANNEL_NAME, R.layout.notification)
 
-        // TODO(developer): Handle FCM messages here.
+        remoteView.setTextViewText(R.id.notificationTextView, notificationMessage)
+        remoteView.setImageViewResource(R.id.logo_for_app, R.mipmap.frisbee_golf_icon_round)
+        return remoteView
+    }
+
+    fun generateNotification(notificationMessage: String ) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        var builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.frisbee_golf_icon_round)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        builder = builder.setContent(getRemoteView(notificationMessage))
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ) {
+            val notificationChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        notificationManager.notify(1, builder.build())
+
+
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d("TAG", "From: ${remoteMessage.from}")
+        Log.d("FCM message", "From: ${remoteMessage.from}")
 
         // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
+            // TODO: Problem, the data is null, and needs to be fixed, but I don't understand why, I can`t figure it out
             Log.d("TAG", "Message data payload: ${remoteMessage.data}")
+            remoteMessage.notification?.body?.let { generateNotification(it) }
 
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-                //scheduleJob()
-            } else {
-                // Handle message within 10 seconds
-                //handleNow()
-            }
         }
 
         // Check if message contains a notification payload.
@@ -31,9 +69,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             Log.d("TAG", "Message Notification Body: ${it.body}")
         }
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
+
 
 
 }
